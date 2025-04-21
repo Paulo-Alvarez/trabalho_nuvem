92dc1c6f404980567f4fa177efdc5d2558e09433
Paulo Alvarez <paulozfilho2@gmail.com>
2025-04-21 20:20:44 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("<h1>Bem-vindo à página inicial!</h1>");
+});
+
+app.get("/sobre", function(req, res) {
+    res.send("<h1>Sobre nós</h1><p>Informações sobre a aplicação.</p>");
+});
+
+app.get("/contato", function(req, res) {
+    res.send("<h1>Contato</h1><p>Entre em contato conosco pelo email paulo@dominio.com.</p>");
+});
+
+app.listen(5000, function (erro) {
+    if (erro) {
+        console.error("Erro ao iniciar o servidor:", erro.message);
+    } else {
+        console.log("Servidor iniciado em http://localhost:5000");
+    }
+});
